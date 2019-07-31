0d255cef1d47a93e33b61bddee12067bdd355318
Illis Draklaw <jean_jacques.poignant@utt.fr>
2019-07-31 22:41:50 +0200

    Création fonctionnelle
    Todo : varier la courbe de difficulté à des degrés visibles

--- Assets/__Scripts/LevelBuilder.cs
+++ Assets/__Scripts/LevelBuilder.cs
@@ -14,11 +14,11 @@ namespace __Scripts
         
         private GameObject _start;
 
-        [Range(10,30)] public int levelSize = 10;
+        [Range(1,30)] public int levelSize = 10;
         [Range(0,1)] public float CR = .5f;
         public Texture2D mapLiberty;
         public GameObject end;
-        private int _blocSize = Mathf.RoundToInt(512*0.2f);
+        private int _blocSize = Mathf.RoundToInt(512);
         
 
         // Start is called before the first frame update
@@ -61,25 +61,130 @@ namespace __Scripts
                         cpt++;
                     }
                 }
-
                 dangerArea /= dangerArea / cpt;
+                
+                //create Accurate % megabloc
                 
                 var targetPercentile = (actualSize*1f / levelSize) * CR;
                 var percentile = dangerArea - targetPercentile; 
                 
-                //instanciation
-                var bInst = Instantiate(blocs[Random.Range(0, blocs.Length)],this.transform);
+                var megaBlocs = new List<GameObject>();
+                for (var i = 0; i < 4; i++)
+                {
+                    var instMega = Instantiate(blocs[Random.Range(0, blocs.Length)],this.transform);
+                    megaBlocs.Add(instMega);
+                    
+                    if (i > 1)
+                    {
+                        var actual = instMega.GetComponent<MetaData>();
+                        var last = megaBlocs[megaBlocs.Count - 1].GetComponent<MetaData>();
+                        //associate blocs in megabloc
+                        var positionToPlace = Place(actual, last, percentile);
+                        PlaceBloc(pos.x+positionToPlace.x,pos.y+positionToPlace.y,instMega);
+                        pos = instMega.transform.position;
+                        Debug.Log(positionToPlace);
+
+                    }
+                    else
+                    {
+                        PlaceBloc(pos.x+1, pos.y,instMega);
+                        pos = instMega.transform.position;
+                    }
+
+                }
                 
-                PlaceBloc(Mathf.FloorToInt(pos.x+1), Mathf.FloorToInt(pos.y), percentile, bInst);
-                pos.x += 1+percentile;
+                
+                //instanciation
+                //var bInst = Instantiate(blocs[Random.Range(0, blocs.Length)],this.transform);
+                
+                //PlaceBloc(Mathf.FloorToInt(pos.x+1), Mathf.FloorToInt(pos.y), percentile, bInst);
+                //pos.x += 1+percentile;
                 actualSize++;
             }
+            PlaceBloc(pos.x+1, pos.y,Instantiate(end));
             FillDeathPits();
 
         }
 
-        private void PlaceBloc(int i, int j, float offset, GameObject bloc)
+        private Vector3 Place(MetaData actual, MetaData last, float target)
         {
+            /**
+             * increments ou 0.5 in position
+             * 512 pix =~ 1
+             *
+             * test from last._pointRight to 2 above/2 further and 2 under
+             *
+             */
+
+            var best = 0f;
+            var bestV = new Vector2(0,0);
+            
+            for (float i = -2; i < 2; i+=0.5f)
+            {
+                for (float j = 0; j < 2; j+=0.5f)
+                {
+                    if (j == 0.5f) continue;
+                    var endPoint = actual._pointLeft;
+                    endPoint.x = endPoint.x + 512 + Mathf.RoundToInt(j * 512);
+                    endPoint.y = endPoint.y + Mathf.RoundToInt(i * 512);
+                    if(!PathAvailable(last._pointRight,endPoint, last._liberty)) continue;
+                    
+                    
+                    //blue % in area
+                    var count = 0f;
+                    if (last._pointRight == endPoint) count = 1f;
+                    else
+                    {
+                        //TODO: remplaçable par le nombre de pixels verts arrivant sur le sol de 2
+                        var k = 0;
+                        var l = 0;
+                        for (k = last._pointRight.x; k <= endPoint.x; k++)
+                        {
+                            for (l = 0; l < _blocSize; l++)
+                            {
+
+                                var pix = mapLiberty.GetPixel(
+                                    Mathf.FloorToInt(i) * _blocSize + k + Mathf.FloorToInt(_blocSize),
+                                    Mathf.FloorToInt(j) * _blocSize + Mathf.FloorToInt(_blocSize));
+                                count += pix.b * pix.a;
+                            }
+                            //mapLiberty.Apply();
+
+                        }
+
+                        count /= (k * l);
+                    }
+
+                    if ((Mathf.Abs(target - best) < Mathf.Abs(target - count))) continue;
+                    best = count; //change 50 to best area variable 
+                    bestV.x = j;
+                    bestV.y = i;
+                }
+            }
+            return bestV;
+        }
+
+        private static bool PathAvailable(Vector2Int depart, Vector2Int fin, Texture2D map)
+        {
+            if (fin.x >= map.width || fin.y >= map.height || fin.y < 0) return false;
+            var pathFound = false;
+            for (var i = depart.x; i < fin.x; i++)
+            {
+                pathFound = false;
+                for (var j = 0; j < map.height; j++)
+                {
+                    var c = map.GetPixel(i, j);
+                    if (!(c.g > 0)) continue;
+                    pathFound = true;
+                    break;
+                }
+                if (!pathFound) break;
+            }
+            return pathFound;
+        }
+        private void PlaceBloc(float i, float j, GameObject bloc)
+        {
+            /*
             var liberty = bloc.GetComponent<MetaData>()._liberty;
             liberty.Resize(Mathf.FloorToInt(liberty.width * 0.2f), Mathf.FloorToInt(liberty.height * 0.2f));
             for (var k = 0; k < liberty.width; k++)
@@ -87,11 +192,13 @@ namespace __Scripts
                 for (var l = 0; l < liberty.height; l++)
                 {
                     var col = liberty.GetPixel(i, j);
-                    col += mapLiberty.GetPixel(i * _blocSize + k + Mathf.FloorToInt((1-offset) * _blocSize), j * _blocSize + l + Mathf.FloorToInt((1-offset) * _blocSize));
-                    mapLiberty.SetPixel(i * _blocSize + k + Mathf.FloorToInt((1-offset) * _blocSize), j * _blocSize + l + Mathf.FloorToInt((1-offset) * _blocSize), col);
+                    col += mapLiberty.GetPixel(i * _blocSize + k + Mathf.FloorToInt((1-offset) * _blocSize), 
+                        j* _blocSize + l + Mathf.FloorToInt((1-offset) * _blocSize));
+                    mapLiberty.SetPixel(i * _blocSize + k + Mathf.FloorToInt((1-offset) * _blocSize), 
+                        j * _blocSize + l + Mathf.FloorToInt((1-offset) * _blocSize), col);
                 }
-            }
-            bloc.transform.position = new Vector3(i+offset,j,0);
+            }*/
+            bloc.transform.position = new Vector3(i/*+offset*/,j,0);
             //mapLiberty.Apply();
         }
         private float CountBloc(float i, float j)
@@ -102,7 +209,8 @@ namespace __Scripts
             {
                 for (l = 0; l < _blocSize; l++)
                 {
-                    var pix = mapLiberty.GetPixel(Mathf.FloorToInt(i) * _blocSize + k + Mathf.FloorToInt(_blocSize), Mathf.FloorToInt(j) * _blocSize + Mathf.FloorToInt(_blocSize));
+                    var pix = mapLiberty.GetPixel(Mathf.FloorToInt(i) * _blocSize + k + Mathf.FloorToInt(_blocSize), 
+                        Mathf.FloorToInt(j) * _blocSize + Mathf.FloorToInt(_blocSize));
                     count += pix.b * pix.a;
                 }
             }
--- Assets/__Scripts/MetaData.cs
+++ Assets/__Scripts/MetaData.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 namespace __Scripts
 {
     [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(PolygonCollider2D))]
     public class MetaData : MonoBehaviour
     {
         /*
@@ -15,9 +17,10 @@ namespace __Scripts
 
         private Vector2 _speedX = new Vector2(50,25);
 
-        private Vector2Int _pointLeft = Vector2Int.zero, _pointRight = Vector2Int.zero;
+        public Vector2Int _pointLeft { get; private set; } = Vector2Int.zero;
+        public Vector2Int _pointRight{ get; private set; } = Vector2Int.zero;
 
-        private float _inertia = 0.3f;
+        private float _friction = 0.3f;
 
         private float _distance;
 
@@ -45,18 +48,20 @@ namespace __Scripts
 
             
             // Initialisation, pas de liberté possible
-            _liberty = new Texture2D(1024,1024,TextureFormat.ARGB32, false);
+            _liberty = new Texture2D(2048,2048,TextureFormat.ARGB32, false);
             for (var xT = 0 ; xT < _liberty.width ; xT++)
             {
                 for (var yT = 0 ; yT < _liberty.height ; yT++) _liberty.SetPixel(xT,yT, new Color(0f,0f,0f,0.5f));
             }
+            _friction = gameObject.GetComponent<PolygonCollider2D>().friction;
             
             /** TODO: [BUG] positionner le bloc sur la texture.
              * Colorier le bloc en blanc 100% transparent sur la texture.
              * Cela permettra de sommer les textures et de n'avoir visible que les degrés de liberté du niveau
              * /!\ lors de la somme, attention à supprimer les projections dépassantes (via une "ombre") 
              */
-            var piece = gameObject.GetComponent<SpriteRenderer>().sprite.texture;
+            Texture2D piece = Instantiate(gameObject.GetComponent<SpriteRenderer>().sprite.texture);
+            //TextureScale.Bilinear(piece,32, 32);
             var w = piece.width;
             var h = piece.height;
             var offsetW = (_liberty.width / 2) - (w / 2);
@@ -79,7 +84,15 @@ namespace __Scripts
                             _pointLeft = new Vector2Int(j, i);
                         }
 
-                        //_liberty.SetPixel(j + offsetH, i + offsetW, new Color(0,0,0,0f));
+                        if (i + 1 < h)
+                        {
+                            if (piece.GetPixel(j, i + 1).a != 0f)
+                            {
+                                _liberty.SetPixel(j + offsetH, i + offsetW, new Color(0,0,0,0f));
+                            }
+                        }
+
+                        
                         //_liberty.SetPixel(j + offsetH, i + offsetW, new Color(i * 1f / h, 0, 0));//j*1f/w,0));
                         _pointRight = new Vector2Int(j,i);
                     }
@@ -142,52 +155,60 @@ namespace __Scripts
                     //Todo: ajout valeures effectives personnage
                     //Courbe de saut et coloriage
                     var startX = pixel.x+1;
-                    var xFunct = startX;
-                    float startY = pixel.y;
-                    var yFunct = startY;
-                    var angleDeg = 45;
-                    var inc = 0;
-                    if (angleDeg > 90) inc = -1;
-                    else inc = 1;
+                    var xFunct = startX+0f;
+                    var startY = pixel.y;
+                    var yFunct = startY+0f;
+                    var angleDeg = 60;
                     var angle = angleDeg * Mathf.PI / 180; //Radians !!
+                    Vector2 norm = _jumpingForce * new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+                    Vector2Int save = new Vector2Int(startX, startY);
 
-                    //Debug.Log(xFunct+" ::: "+yFunct); //aff0
                     while (xFunct < _liberty.width && xFunct >= 0 && yFunct < _liberty.height && yFunct >= 0)
                     {
-                        var color2 = _liberty.GetPixel(xFunct, Mathf.RoundToInt(yFunct));
-                        if (color2.a == 0f) break;
-                        color2.g += (_speedX.x / 255);
-                        //color2.r += (_speedX.y / 255);
-                        //color2.b += 50;// (_jumpingForce/255);
-                        _liberty.SetPixel(xFunct, Mathf.RoundToInt(yFunct), color2);
-                        xFunct += inc;
-                        yFunct = -0.5f * (Mathf.Pow(xFunct - startX, 2) * _gravity /
-                                          (Mathf.Pow(Mathf.Cos(angle), 2) * Mathf.Pow(_jumpingForce, 2))) +
-                                 Mathf.Tan(angle) * (xFunct - startX) + startY;
-                    }
+                        norm.x = _jumpingForce * Mathf.Cos(angle);
+                        norm.y = _jumpingForce * Mathf.Sin(angle) - (_gravity*(xFunct-startX)/(norm.x));
+                        norm = norm.normalized;
+                        yFunct += norm.y;
+                        xFunct += norm.x;
 
+                        if (save.x != Mathf.RoundToInt(xFunct) || save.y != Mathf.RoundToInt(yFunct))
+                        {
+                            save.x = Mathf.RoundToInt(xFunct);
+                            save.y = Mathf.RoundToInt(yFunct);
+                            var color2 = _liberty.GetPixel(save.x, save.y);
+                            if (color2.a == 0f) break;
+                            color2.g += (_speedX.x / 255);    
+                            _liberty.SetPixel(save.x, save.y, color2);
+                            
+                        }
+                    }
+                    
                     startX = pixel.x+1;
                     xFunct = startX;
                     startY = pixel.y;
                     yFunct = startY;
-                    angleDeg = 135;
-                    if (angleDeg > 90) inc = -1;
-                    else inc = 1;
+                    angleDeg = 120;
                     angle = angleDeg * Mathf.PI / 180; //Radians !!
-
-                    //Debug.Log(xFunct+" ::: "+yFunct); //aff0
+                    norm = _jumpingForce * new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+                    save = new Vector2Int(startX, startY);
                     while (xFunct < _liberty.width && xFunct >= 0 && yFunct < _liberty.height && yFunct >= 0)
                     {
-                        var color2 = _liberty.GetPixel(xFunct, Mathf.RoundToInt(yFunct));
-                        if (color2.a == 0f) break;
-                        //color2.g += (_speedX.x / 255);
-                        color2.r += (_speedX.y / 255);
-                        //color2.b += 50;// (_jumpingForce/255);
-                        _liberty.SetPixel(xFunct, Mathf.RoundToInt(yFunct), color2);
-                        xFunct += inc;
-                        yFunct = -0.5f * (Mathf.Pow(xFunct - startX, 2) * _gravity /
-                                          (Mathf.Pow(Mathf.Cos(angle), 2) * Mathf.Pow(_jumpingForce, 2))) +
-                                 Mathf.Tan(angle) * (xFunct - startX) + startY;
+                        norm.x = _jumpingForce * Mathf.Cos(angle);
+                        norm.y = _jumpingForce * Mathf.Sin(angle) - (_gravity*(xFunct-startX)/(norm.x));
+                        norm = norm.normalized;
+                        yFunct += norm.y;
+                        xFunct += norm.x;
+
+                        if (save.x != Mathf.RoundToInt(xFunct) || save.y != Mathf.RoundToInt(yFunct))
+                        {
+                            save.x = Mathf.RoundToInt(xFunct);
+                            save.y = Mathf.RoundToInt(yFunct);
+                            var color2 = _liberty.GetPixel(save.x, save.y);
+                            if (color2.a == 0f) break;
+                            color2.r += (_speedX.x / 255);    
+                            _liberty.SetPixel(save.x, save.y, color2);
+                            
+                        }
                     }
 
                     //Todo: Air Control
@@ -201,7 +222,7 @@ namespace __Scripts
             }
             
             //Inertia
-            for (int i = 0; i < Mathf.FloorToInt(_speedX.x * _inertia); i++)
+            for (int i = 0; i < Mathf.FloorToInt(_speedX.x * _friction); i++)
             {
                 for (int j = 0; j < _pointLeft.y; j++)
                 {
@@ -240,7 +261,7 @@ namespace __Scripts
             //Debug.Log("GUI of " + this.name + " : " + (_liberty != null));//aff0
             if (_liberty)
             {
-                GUI.DrawTexture(new Rect(new Vector2(0,0),new Vector2(500,500)), _liberty);
+                //GUI.DrawTexture(new Rect(new Vector2(0,0),new Vector2(500,500)), _liberty);
             }
         }
 /*
